9330aa30cfbacbcce38eb0a723eb2b3821fb22c0
Matheus Mondaini Alegre de Miranda <matheus.mondaini@outlook.com>
2025-03-05 23:51:05 -0300

    fix: enable trailing slash and remove strict react

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
     basePath: process.env.NODE_ENV === 'production' ? '/task-manager' : '',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/task-manager/' : '',
     images: {
       unoptimized: true,
     },
     output: 'export',
+    trailingSlash: true,
 };
 
 export default nextConfig;
