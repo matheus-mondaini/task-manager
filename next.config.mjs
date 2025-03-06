c12e133062fc41b32853fcdbf877df73472307f0
Matheus Mondaini Alegre de Miranda <matheus.mondaini@outlook.com>
2025-03-05 23:30:58 -0300

    chore: configure for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    basePath: process.env.NODE_ENV === 'production' ? '/task-manager' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/task-manager/' : '',
+    images: {
+      unoptimized: true,
+    },
+    output: 'export',
+  }
+  
+  module.exports = nextConfig
 
 export default nextConfig;
