0c66249c528e921fa1ece40eb553db52f1b6d09e
Matheus Mondaini Alegre de Miranda <matheus.mondaini@outlook.com>
2025-03-06 22:46:15 -0300

    docs: only docs one more unsuccessful try to fix tailwindcss rendering in github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.NODE_ENV === 'production' ? '/task-manager' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/task-manager/' : '',
-    images: {
-      unoptimized: true,
-    },
-    output: 'export',
-    trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/task-manager' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/task-manager/' : '',
+  images: {
+    unoptimized: true,
+  },
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
